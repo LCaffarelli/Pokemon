6949430411f74b2108e352c4e96be08b1f26b4ab
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-03-21 10:01:50 +0100

    ajout de fichiers non versionés

---
+++ src/Form/PokemonFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pokemon;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PokemonFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('attaque')
+            ->add('defense')
+            ->add('estCapture')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pokemon::class,
+        ]);
+    }
+}
